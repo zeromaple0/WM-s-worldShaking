b1acdcf123050395185480d146b7312cc3706363
bldst <1677931886@qq.com>
2024-09-16 22:30:10 +0800

    更改getprimeprice 为post提交 修改views中的ALLOWED_HOSTS为所有

--- mysite/django_api/views.py
+++ mysite/django_api/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .PythonBackend import get_and_print_prime_orders
 from .PythonBackend import get_and_print_riven_orders
 
@@ -10,20 +10,32 @@ def index(request):
 
 
 def getprimeprice(request):  # 获得prime部件价格
-    # 解析request.GET
-    search_online = False
-    prime_url_name_list = []
-    prime_url_name_list = request.GET.get('prime_url_name_list')
-    print("得到的prime_url_name_list为：" + prime_url_name_list)
-    if request.GET.get('search_online') != None:
-        search_online = request.GET.get('search_online', False)
-        print("得到的search_online为：" + search_online)
-    price = get_and_print_prime_orders.PrimeOrdersProcess(prime_url_name_list, search_online)
-    # 把price.prime_dict转为json
-    json_data = json.dumps(price.prime_dict)
+    if request.method=='POST':
+        try:
+            #从post 中获取数据
+            data=json.loads(request.body)
+            prime_url_name_list = data.get('prime_url_name_list')
+            search_online = data.get('search_online', False)
+            # 打印接收到的参数
+            print("得到的prime_url_name_list为：" + str(prime_url_name_list))
+            print("得到的search_online为：" + str(search_online))
 
-    print(json_data)
-    return HttpResponse(json_data)
+            # 开始处理订单
+            price = get_and_print_prime_orders.PrimeOrdersProcess(prime_url_name_list, search_online)
+            # 把price.prime_dict转为json
+            json_data = json.dumps(price.prime_dict)
+
+            # 返回JSON响应
+            return JsonResponse(json_data, safe=False)
+        except json.JSONDecodeError as e:
+            # 处理JSON解码错误
+            return JsonResponse({'error': 'Invalid JSON format'}, status=400)
+        except Exception as e:
+            # 处理其他可能的错误
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        # 错误的方法类型
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 
 def getrivenprice(request):
