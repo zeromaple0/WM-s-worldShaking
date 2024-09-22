6d684c9dc1dbf0dda64388fb2e6f69b446ae1ed4
zeromaple0 <1245306335@qq.com>
2024-09-22 23:25:52 +0800

    将序列化实例输出至Text文本

--- CsharpUI/UI/Assets/Scripts/TestJson.cs
+++ CsharpUI/UI/Assets/Scripts/TestJson.cs
@@ -18,6 +18,10 @@ public class OrdersContainer
 public class TestJson : MonoBehaviour
 {
     public Button button;
+    public Text textOrder1;
+    public Text textOrder2;
+    //public Text textOrder3;
+
 
     private void Start()
     {
@@ -38,11 +42,11 @@ public class TestJson : MonoBehaviour
                     ]
                 },
                 {
-                    ""weapon_url"": ""magistar"",
+                    ""weapon_url"": ""sanbaoju"",
                     ""price_list"": [
-                        500,
-                        550,
-                        550
+                        50,
+                        60,
+                        70
                     ]
                 }
             ]
@@ -51,9 +55,44 @@ public class TestJson : MonoBehaviour
         // 解析JSON字符串
         OrdersContainer container = JsonUtility.FromJson<OrdersContainer>(jsonString);
 
-        foreach (var order in container.orders)
+
+        // 清空之前的文本内容
+        textOrder1.text = "";
+        textOrder2.text = "";
+
+        // 遍历订单数据并更新UI控件
+        if (container.orders.Length >= 1)
         {
-            Debug.Log($"Order Name: {order.weapon_url}, Prices: [{order.price_list[0]}, {order.price_list[1]}, {order.price_list[2]}]");
+            UpdateUITexts(container.orders[0], textOrder1);
+        }
+        if (container.orders.Length >= 2)
+        {
+            UpdateUITexts(container.orders[1], textOrder2);
+        }
+        // foreach (var order in container.orders)
+        // {
+        //     Debug.Log($"Order Name: {order.weapon_url}, Prices: [{order.price_list[0]}, {order.price_list[1]}, {order.price_list[2]}]");
+        //     string text = $"Order Name: {order.weapon_url}, Prices: [{order.price_list[0]}, {order.price_list[1]}, {order.price_list[2]}]";
+        //     printText.text = text;
+        // }
+    }
+    private void UpdateUITexts(Order order,Text text)
+    {
+         if (order != null)
+        {
+            string orderInfo = $"Weapon URL: {order.weapon_url}\n";
+            string priceListText = "Prices: [";
+            for (int i = 0; i < order.price_list.Length; i++)
+            {
+                priceListText += $"{order.price_list[i]}";
+                if (i < order.price_list.Length - 1)
+                {
+                    priceListText += ", ";
+                }
+            }
+            priceListText += "]";
+
+            text.text = orderInfo + priceListText;
         }
     }
 }
